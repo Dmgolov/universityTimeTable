fdddfe04ce89f63e8f9a40625d81bc2507db55ce
imgret <imgret@ttu.ee>
2018-11-18 11:07:28 +0200

    Added in Timetable to fitness calculation time gaps evaluation, added to Timetable search for subject by id, added to Timetable maximum possible fitness calculation and get method for it, added to Timetable method for replacing current timetable's subject to given new subject (subjects' names must be identical).

--- src/main/java/genetic/timetable/Timetable.java
+++ src/main/java/genetic/timetable/Timetable.java
@@ -4,13 +4,18 @@ import genetic.storage.SubjectStorage;
 import genetic.subjects.Subject;
 import genetic.util.Utility;
 
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
 
 public class Timetable {
 
-    private HashMap<Integer, List<Subject>> days = new HashMap<>();
+    // subjects are used as genes
+    private LinkedHashMap<Integer, List<Subject>> days = new LinkedHashMap<>();
 
-    private long fitness = 0;
+    private int fitness = 0;
+
+    private int maxFitness;
 
     public Timetable() {
         // initialize lists in days HashMap
@@ -19,15 +24,52 @@ public class Timetable {
         }
     }
 
-    public long getFitness() {
+    public int getFitness() {
         return fitness;
+    }
+
+    public Subject getSubjectByIndex(int index) {
+        int counter = 0;
+        for (Map.Entry<Integer, List<Subject>> day : days.entrySet()) {
+            List<Subject> subjects = day.getValue();
+            for (Subject subject : subjects) {
+                if (counter == index) {
+                    return subject;
+                } else {
+                    counter++;
+                }
+            }
+        }
+        return new Subject("Subject not found by index", 0,
+                LocalTime.of(0, 0), LocalTime.of(0, 0));
+    }
+
+    // replaces subject located in timetable with given subject and returns old subject (which was in timetable)
+    public Subject replaceSubjectWithGiven(Subject subject) {
+        for (Map.Entry<Integer, List<Subject>> day : days.entrySet()) {
+            List<Subject> subjects = day.getValue();
+            for (int i = 0; i < subjects.size(); i++) {
+                Subject currentSubject = subjects.get(i);
+                if (currentSubject.getName().equals(subject.getName())) {
+                    days.get(currentSubject.getDay()).remove(i);
+                    days.get(currentSubject.getDay()).sort(Comparator.comparing(Subject::getStart));
+                    days.get(subject.getDay()).add(subject);
+                    days.get(subject.getDay()).sort(Comparator.comparing(Subject::getStart));
+                    return currentSubject;
+                }
+            }
+        }
+        return new Subject("Subject is missing", 0,
+                LocalTime.of(0, 0), LocalTime.of(0, 0));
     }
 
     public void calculateFitness() {
         fitness = 0;
         int subjectsNumber = 0;
-        long maxConflicts;
-        long totalConflicts = 0;
+        int maxConflicts;
+        int totalConflicts = 0;
+        int maxTimeGaps;
+        int totalTimeGaps = 0;
         for (List<Subject> day : days.values()) {
             long dayConflicts = 0;
             for (int i = 0; i < day.size(); i++) {
@@ -36,16 +78,31 @@ public class Timetable {
                     // check if subjects are overlapping
                     // (if s1 end is 10:30 and s2 start is 10:30 then they are not overlapping)
                     // used https://stackoverflow.com/questions/17106670/how-to-check-a-timeperiod-is-overlapping-another-time-period-in-java
-                    if (day.get(i).getStart().isBefore(day.get(j).getEnd()) &&
-                            day.get(j).getStart().isBefore(day.get(i).getEnd())) {
+                    if (day.get(i).getStart().isBefore(day.get(j).getBreakEnd()) &&
+                            day.get(j).getStart().isBefore(day.get(i).getBreakEnd())) {
                         dayConflicts++;
+                    }
+                    // compare subject and it's next subject if between them is time gap
+                    // (compare only not conflicting subjects)
+                    // used https://stackoverflow.com/questions/28353725/java-subtract-localtime
+                    else if (j == i + 1 &&
+                            ChronoUnit.SECONDS.between(day.get(i).getBreakEnd(), day.get(j).getStart()) > 0) {
+                        totalTimeGaps++;
                     }
                 }
             }
             totalConflicts += dayConflicts;
         }
-        maxConflicts = Utility.findCombinationsNumber(subjectsNumber, 2).longValue();
-        fitness = maxConflicts - totalConflicts;
+        maxConflicts = Utility.findCombinationsNumber(subjectsNumber, 2).intValue();
+        maxTimeGaps = subjectsNumber -1;
+        fitness = maxConflicts + maxTimeGaps - totalConflicts - totalTimeGaps;
+        maxFitness = maxConflicts + maxTimeGaps;
+        // System.out.println(maxConflicts + ", " + maxTimeGaps + ", " + totalConflicts + ", " + totalTimeGaps);
+    }
+
+    public int getMaxFitness() {
+        calculateFitness();
+        return maxFitness;
     }
 
     public void initializeRandomTimetable(SubjectStorage storage) {
@@ -77,9 +134,10 @@ public class Timetable {
         for (Map.Entry<Integer, List<Subject>> day : days.entrySet()) {
             List<Subject> schedule = day.getValue();
             for (Subject subject : schedule) {
-                System.out.printf("%d %tH:%tM - %tH:%tM   %s \n",
+                System.out.printf("%d %tH:%tM - %tH:%tM - %tH:%tM   %s\n",
                         subject.getDay(), subject.getStart(), subject.getStart(),
-                        subject.getEnd(), subject.getEnd(), subject.getName());
+                        subject.getEnd(), subject.getEnd(),
+                        subject.getBreakEnd(), subject.getBreakEnd(), subject.getName());
             }
             System.out.println("-------");
         }
