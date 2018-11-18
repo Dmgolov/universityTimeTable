a67e75177085d882cc38438284064bd69980d8af
imgret <imgret@ttu.ee>
2018-11-18 11:12:25 +0200

    Added to Population variable for SubjectStorage, added method for getting first fittest timetable from population, added method for getting second fittest timetable from population, added method for getting least fittest timetable from population, added method to Population for all timetables fitness calculation and it is also updating fittestValue variable.

--- src/main/java/genetic/population/Population.java
+++ src/main/java/genetic/population/Population.java
@@ -5,20 +5,87 @@ import genetic.timetable.Timetable;
 
 public class Population {
 
-    private int populationSize = 10;
-    private Timetable[] population = new Timetable[populationSize];
-    private int fittest = 0;
+    private int populationSize;
+    private Timetable[] timetables;
+    private int fittestValue = 0;
     private SubjectStorage storage;
 
-    public Population(SubjectStorage storage) {
+    public Population(int populationSize, SubjectStorage storage) {
+        this.populationSize = populationSize;
+        this.timetables = new Timetable[populationSize];
         this.storage = storage;
     }
 
-    public void initializePopulation(int size) {
-        for (int i = 0; i < population.length; i++) {
-            population[i] = new Timetable();
+    public int getSize() {
+        return populationSize;
+    }
+
+    public Timetable[] getTimetables() {
+        return timetables;
+    }
+
+    public void setTimetables(Timetable[] timetables) {
+        this.timetables = timetables;
+    }
+
+    public int getFittestValue() {
+        return fittestValue;
+    }
+
+    public SubjectStorage getStorage() {
+        return storage;
+    }
+
+    public void initializePopulation() {
+        for (int i = 0; i < timetables.length; i++) {
+            timetables[i] = new Timetable();
+            timetables[i].initializeRandomTimetable(storage);
         }
     }
 
+    public Timetable getFittest() {
+        int maxFittestValue = Integer.MIN_VALUE;
+        int maxFittestIndex = 0;
+        for (int i = 0; i < timetables.length; i++) {
+            if (maxFittestValue <= timetables[i].getFitness()) {
+                maxFittestValue = timetables[i].getFitness();
+                maxFittestIndex = i;
+            }
+        }
+        fittestValue = timetables[maxFittestIndex].getFitness();
+        return timetables[maxFittestIndex];
+    }
 
+    public Timetable getSecondFittest() {
+        int maxFittestFirst = 0;
+        int maxFittestSecond = 0;
+        for (int i = 0; i < timetables.length; i++) {
+            if (timetables[i].getFitness() > timetables[maxFittestFirst].getFitness()) {
+                maxFittestSecond = maxFittestFirst;
+                maxFittestFirst = i;
+            } else if (timetables[i].getFitness() > timetables[maxFittestSecond].getFitness()) {
+                maxFittestSecond = i;
+            }
+        }
+        return timetables[maxFittestSecond];
+    }
+
+    public int getLeastFittestIndex() {
+        int minFitnessValue = Integer.MAX_VALUE;
+        int minFitnessIndex = 0;
+        for (int i = 0; i < timetables.length; i++) {
+            if (minFitnessValue >= timetables[i].getFitness()) {
+                minFitnessValue = timetables[i].getFitness();
+                minFitnessIndex = i;
+            }
+        }
+        return minFitnessIndex;
+    }
+
+    public void calculateFitness() {
+        for (int i = 0; i < timetables.length; i++) {
+            timetables[i].calculateFitness();
+        }
+        getFittest();
+    }
 }
