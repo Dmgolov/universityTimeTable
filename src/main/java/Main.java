627f35828d3ba28ab9887b7f678038f75699d652
imgret <imgret@ttu.ee>
2018-11-18 11:16:46 +0200

    Main class for static main method, added there usage of Genetic algorithm

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import genetic.algorithm.GeneticAlgorithm;
 import genetic.storage.SubjectStorage;
 import genetic.subjects.Subject;
 import genetic.timetable.Timetable;
@@ -8,24 +9,32 @@ public class Main {
     public static void main(String[] args) {
 
         SubjectStorage storage = new SubjectStorage();
-        storage.addSubject(new Subject("Mathematics", 0, LocalTime.of(10, 0), LocalTime.of(11, 30)));
-        storage.addSubject(new Subject("Mathematics", 2, LocalTime.of(12, 0), LocalTime.of(13, 30)));
-        storage.addSubject(new Subject("Mathematics", 2, LocalTime.of(17, 30), LocalTime.of(19, 0)));
-        storage.addSubject(new Subject("Mathematics", 3, LocalTime.of(8, 0), LocalTime.of(9, 30)));
-        storage.addSubject(new Subject("Java", 0, LocalTime.of(10, 0), LocalTime.of(11, 30)));
-        storage.addSubject(new Subject("Java", 3, LocalTime.of(8, 0), LocalTime.of(9, 30)));
-        storage.addSubject(new Subject("Enesejuhtimine", 3, LocalTime.of(18, 0), LocalTime.of(19, 30)));
-        storage.addSubject(new Subject("Materjalid", 3, LocalTime.of(18, 0), LocalTime.of(19, 30)));
-        storage.addSubject(new Subject("Veebiteenused", 3, LocalTime.of(18, 0), LocalTime.of(19, 30)));
-        storage.addSubject(new Subject("Veebiteenused", 4, LocalTime.of(18, 0), LocalTime.of(19, 30)));
-        storage.addSubject(new Subject("Python algkursus", 3, LocalTime.of(18, 0), LocalTime.of(19, 30)));
-        storage.addSubject(new Subject("Python algkursus", 0, LocalTime.of(10, 0), LocalTime.of(11, 30)));
+        storage.addSubject(new Subject("Mathematics", 0, LocalTime.of(10, 0), LocalTime.of(11, 30), LocalTime.of(12, 0)));
+        storage.addSubject(new Subject("Mathematics", 2, LocalTime.of(12, 0), LocalTime.of(13, 30), LocalTime.of(14, 0)));
+        storage.addSubject(new Subject("Mathematics", 2, LocalTime.of(17, 30), LocalTime.of(19, 0), LocalTime.of(19, 30)));
+        storage.addSubject(new Subject("Mathematics", 3, LocalTime.of(8, 0), LocalTime.of(9, 30), LocalTime.of(10, 0)));
+        storage.addSubject(new Subject("Java", 0, LocalTime.of(10, 0), LocalTime.of(11, 30), LocalTime.of(12, 0)));
+        storage.addSubject(new Subject("Java", 3, LocalTime.of(8, 0), LocalTime.of(9, 30), LocalTime.of(10, 0)));
+        storage.addSubject(new Subject("Enesejuhtimine", 3, LocalTime.of(18, 0), LocalTime.of(19, 30), LocalTime.of(20, 0)));
+        storage.addSubject(new Subject("Materjalid", 3, LocalTime.of(18, 0), LocalTime.of(19, 30), LocalTime.of(20, 0)));
+        storage.addSubject(new Subject("Veebiteenused", 3, LocalTime.of(18, 0), LocalTime.of(19, 30), LocalTime.of(20, 0)));
+        storage.addSubject(new Subject("Veebiteenused", 4, LocalTime.of(18, 0), LocalTime.of(19, 30), LocalTime.of(20, 0)));
+        storage.addSubject(new Subject("Python algkursus", 3, LocalTime.of(18, 0), LocalTime.of(19, 30), LocalTime.of(20, 0)));
+        storage.addSubject(new Subject("Python algkursus", 0, LocalTime.of(10, 0), LocalTime.of(11, 30), LocalTime.of(12, 0)));
+        storage.addSubject(new Subject("Physics", 2, LocalTime.of(16, 0), LocalTime.of(17, 30)));
 
-        Timetable timetable = new Timetable();
-        timetable.initializeRandomTimetable(storage);
-        timetable.printTimetable();
-        timetable.calculateFitness();
-        System.out.println(timetable.getFitness());
+//        Timetable timetable = new Timetable();
+//        timetable.initializeRandomTimetable(storage);
+//        timetable.printTimetable();
+//        timetable.calculateFitness();
+//        System.out.println(timetable.getFitness());
+
 //        System.out.println((char)27 + "[31m" + "ERROR MESSAGE IN RED");
+//        System.out.println(ChronoUnit.SECONDS.between(LocalTime.of(10, 30), LocalTime.of(10, 30)));
+
+
+
+        GeneticAlgorithm algorithm = new GeneticAlgorithm(1000, 100, storage);
+        Timetable timetable = algorithm.findFittestTimetable();
     }
 }
